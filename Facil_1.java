209f3df998c0cf942e1ae6b2dd8f563828ee3e94
Pedro Cícero Oliveira Bitsck <pedrocbitsck10@gmail.com>
2025-09-17 20:16:01 -0300

    Add files via upload

---
+++ Facil_1.java
@@ -0,0 +1,12 @@
+package com.mycompany.aula_9;
+public class Facil_1 {
+    public static void main(String[] args) {
+        int[]numeros = {1,2,3,4,5};
+        int soma = 0;
+        for(int num : numeros){
+            soma += num;
+        }
+        System.out.println("A soma dos elementos é "+soma);
+    }
+    
+}
